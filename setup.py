b654425f70c52ab72823eabb0088bfcc6ec285cd
ayakau <104378324+ayakau@users.noreply.github.com>
2023-03-02 12:21:20 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="TensorDec",
+    name="model",
     version="0.0.1",
     author="chimi",
     author_email="abc@example.com",
