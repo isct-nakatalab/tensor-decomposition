7b3865a4e7ac389be1273aebcf041fd48b29cc62
ayakau <104378324+ayakau@users.noreply.github.com>
2023-03-02 12:24:41 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="model",
+    name="src",
     version="0.0.1",
     author="chimi",
     author_email="abc@example.com",
@@ -17,8 +17,8 @@ setuptools.setup(
     long_description=long_description,
     url="https://github.com/tokyotech-nakatalab/tensor_decomposition",
     install_requires=_requires_from_file("requirements.txt"),
-    packages=setuptools.find_packages("./src"),
-    package_dir={"": "src"},
+    packages=setuptools.find_packages("."),
+    package_dir={"": "."},
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
